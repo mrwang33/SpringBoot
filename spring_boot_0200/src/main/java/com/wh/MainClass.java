3fd60d773827f9b76c210d0bb85c30058146c39f
wh <wang1386528@gmail.com>
2017-06-29 23:38:29 +0800

    spring boot

---
+++ spring_boot_0200/src/main/java/com/wh/MainClass.java
@@ -0,0 +1,14 @@
+package com.wh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by wh on 17-6-29.
+ */
+@SpringBootApplication
+public class MainClass {
+    public static void main(String[] args) {
+        SpringApplication.run(MainClass.class,args);
+    }
+}
---
+++ spring_boot_0200/src/main/java/com/wh/controller/MyController.java
@@ -0,0 +1,16 @@
+package com.wh.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by wh on 17-6-29.
+ */
+@RestController
+public class MyController {
+
+    @RequestMapping("/index")
+    public Object 你好啊() {
+        return "hello world";
+    }
+}
