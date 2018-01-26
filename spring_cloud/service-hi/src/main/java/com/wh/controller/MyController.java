98b482a7de0cc0f4b9f88fba39647d5ea9712a29
wang33 <wang1386528@gmail.com>
2018-01-26 10:24:06 +0800

    spring cloud service

---
+++ spring_cloud/service-hi/src/main/java/com/wh/controller/MyController.java
@@ -0,0 +1,13 @@
+package com.wh.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @RequestMapping("/index")
+    public String index() {
+        return "hello spring cloud";
+    }
+}
