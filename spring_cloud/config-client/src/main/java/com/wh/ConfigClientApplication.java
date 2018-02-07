7f0fcc6a1c46973bd2a161588b940a259054fc80
wang33 <wang1386528@gmail.com>
2018-02-07 11:57:17 +0800

    消息总线

--- spring_cloud/config-client/src/main/java/com/wh/ConfigClientApplication.java
+++ spring_cloud/config-client/src/main/java/com/wh/ConfigClientApplication.java
@@ -2,6 +2,7 @@ package com.wh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 public class ConfigClientApplication {
