8e53d6544af722a013031bfca1e19b7f8c24c949
wang33 <wang1386528@gmail.com>
2018-02-12 13:42:48 +0800

    product

---
+++ spring_cloud/springcloud-demo/springcloud-common/src/main/java/com/wh/entity/Product.java
@@ -0,0 +1,36 @@
+package com.wh.entity;
+
+import java.io.Serializable;
+
+public class Product implements Serializable {
+
+    private Integer productId;
+
+    private String productName;
+
+    private String description;
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ spring_cloud/springcloud-demo/springcloud-common/src/main/java/com/wh/mapper/ProductMapper.java
@@ -0,0 +1,11 @@
+package com.wh.mapper;
+
+import com.wh.entity.Product;
+
+import java.util.List;
+
+public interface ProductMapper {
+
+    List<Product> selectAll();
+
+}
