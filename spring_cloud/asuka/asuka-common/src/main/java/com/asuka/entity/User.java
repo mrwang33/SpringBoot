7096b765e9bdbbf2f7b3b02d787c737c90025dbd
wang33 <wang1386528@gmail.com>
2018-04-08 19:13:02 +0800

    asuka

---
+++ spring_cloud/asuka/asuka-common/src/main/java/com/asuka/entity/User.java
@@ -0,0 +1,75 @@
+package com.asuka.entity;
+
+import java.util.Date;
+
+public class User {
+    private Integer userId;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private Integer gender;
+
+    private Date registerDate;
+
+    private String phone;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public Date getRegisterDate() {
+        return registerDate;
+    }
+
+    public void setRegisterDate(Date registerDate) {
+        this.registerDate = registerDate;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+}
